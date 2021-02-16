6324d578089e9f356b816a1021f83f59b53a29b8
Carlos Ocana <carlosocana91@gmail.com>
2021-02-16 18:13:15 +0100

    Subido primera parte

---
+++ mobile.ts
@@ -0,0 +1,26 @@
+export class Mobile
+{
+    public name:string;
+    public model:string;
+    public trademark:string;
+    public sdSize:string;
+    public color:string;
+    public is5G:boolean;
+    public cameraNumber:number;
+    public price:number;
+
+    constructor(name:string,model:string,trademark:string,sdSize:string,
+        color:string,is5G:boolean,cameraNumber:number, price:number)
+        {
+            this.name=name;
+            this.model=model;
+            this.trademark = trademark;
+            this.sdSize = sdSize;
+            this.color=color;
+            this.is5G=is5G;
+            this.cameraNumber=cameraNumber;
+            this.price=price;
+        }
+}
+
+
---
+++ mobileTest.ts
@@ -0,0 +1,17 @@
+import {Mobile} from "./mobile";
+
+let mobile1:Mobile = new Mobile ("Nokia3210", "3210", "Nokia", "big", "gray", true, 2, 500);
+
+let mobile2:Mobile = new Mobile ("iPhone3G", "3G", "Apple", "small", "black", false, 3, 1000);
+
+let mobile3:Mobile = new Mobile ("Samsung Galaxy 10", "Galaxy 10", "Samsung", "medium", "white", false, 1, 899 );
+
+
+let mobiles: Mobile[] = [mobile1, mobile2, mobile3];
+
+
+mobile2.is5G = true;
+
+mobile2.cameraNumber = 4;
+
+console.log(mobiles);
